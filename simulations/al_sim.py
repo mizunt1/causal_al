dd56d14f45e444b1219e0b9e7a3035c3bff5e273
mizunt1 <mizunt@gmail.com>
2023-07-04 11:20:28 -0400

    arg parse

--- simulations/al_sim.py
+++ simulations/al_sim.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
+from argparse import ArgumentParser
 
 import torch.nn as nn
-import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
+
 from data_gen import scm1_data, scm_ac_data
 
 class Model(nn.Module):
@@ -23,7 +24,7 @@ class Model(nn.Module):
         return x
 
 
-def train(num_epochs, model, data, target, lr, log_interval=100):
+def train(num_epochs, model, data, target, lr, device,  log_interval=100):
     model.train()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     for epoch in range(num_epochs):
@@ -40,20 +41,20 @@ def train(num_epochs, model, data, target, lr, log_interval=100):
             print('Train Epoch: {} Loss: {:.3f} Train correct: {:.3f}' .format(
                 epoch, loss.item(), correct))
 
-def test(model, data, target):
+def test(model, data, target, device):
     model.eval()
     data, target = data.to(device), target.to(device)
     output = model(data)
     accuracy = torch.sum(torch.argmax(output, axis=1) == target) 
     return accuracy
 
-if __name__ == '__main__':
+def main(args):
     seed = 3
     torch.manual_seed(seed)
     device = torch.device('cuda')
-    sem = "sem1"
-    option = "equal"
-    plot = False
+    sem = args.sem
+    option = args.option
+    plot = args.plot
     if sem == "sem1":
         data, target, data_test, target_test = scm1_data(option, seed)
         if plot:
@@ -62,7 +63,7 @@ if __name__ == '__main__':
             plt.xlabel("non causal variable")
             plt.scatter(data[:,0], data[:,1])
             plt.show()
-            
+
             plt.axhline(y=0.5, color='r')
             plt.ylabel("causal variable")
             plt.xlabel("non causal variable")
@@ -75,11 +76,17 @@ if __name__ == '__main__':
 
     # MODEL SETUP
     model = Model(input_size).to(device)
-    test_accuracy = test(model, data_test, target_test)
+    test_accuracy = test(model, data_test, target_test, device)
     print('test accuracy before training: {}'.format(test_accuracy))
     log_interval = 1
-    
-    train(1000, model, data, target, 1e-2)
-        
+
+    train(1000, model, data, target, 1e-2, device)
+
     test_accuracy = test(model, data_test, target_test)
     print('test accuracy: {}'.format(test_accuracy))
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--scm', type=str, choices=['scm1', 'scm_ac'])
+    parser.add_argument('--option', type=str)
+    parser.add_argument('--plot', action='store_true')
